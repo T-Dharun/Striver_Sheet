61ecea4b43729753887db99927427f0c6976d198
T-Dharun <dharuneee007@gmail.com>
2023-11-29 18:48:08 +0530

    Sorting Attached

---
+++ 2.Sorting/3.InsertionSort.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public static void insertionSort(int[] arr, int size) {
+        int n=size;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+    }
+}
