27f4e0b52eecbf28220704b5cb5ca60447025060
T-Dharun <dharuneee007@gmail.com>
2023-12-22 10:32:46 +0530

    Array Attached

---
+++ 3.Array/1.Easy/20.FindMissingNum2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        int total=(n*(n+1))/2;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        return total-sum;
+    }
+}
