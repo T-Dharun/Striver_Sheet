a993f2874adc071e88add160645daa5e78221c1b
T-Dharun <dharuneee007@gmail.com>
2023-12-24 18:02:19 +0530

    Array Attached

---
+++ 3.Array/2.Medium/1.TwoSum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Solution {
+    public static String read(int n, int []book, int target){
+        HashMap<Integer,Integer> hash=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int num=book[i];
+            int ch=target-num;
+            if(hash.containsKey(ch)){
+                return "YES";
+            }
+            hash.put(num,0);
+        }
+        return "NO";
+    }
+}
