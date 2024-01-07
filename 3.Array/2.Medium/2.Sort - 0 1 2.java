74a84ed4b9be89de2f6670eed70d4982e324128a
T-Dharun <dharuneee007@gmail.com>
2024-01-07 11:31:51 +0530

    Updated

--- 3.Array/2.Medium/2.Sort - 0 1 2.java
+++ 3.Array/2.Medium/2.Sort - 0 1 2.java
@@ -1,15 +1,29 @@
-import java.util.*;
-public class Solution {
-    public static String read(int n, int []book, int target){
-        HashMap<Integer,Integer> hash=new HashMap<>();
-        for(int i=0;i<n;i++){
-            int num=book[i];
-            int ch=target-num;
-            if(hash.containsKey(ch)){
-                return "YES";
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int left=0,right=n-1;
+        if(n==1) return ;
+        int i=0;
+        while(i<=right){
+            if(nums[i]==0){
+                int temp=nums[left];
+                nums[left]=nums[i];
+                nums[i]=temp;
+                left++;
+                i++;
             }
-            hash.put(num,i);
+            else if(nums[i]==1){
+                //left++;
+                i++;
+            }
+            //if(nums[i]==2){
+            else{
+                int temp=nums[right];
+                nums[right]=nums[i];
+                nums[i]=temp;
+              //  i++;
+             right--;
+            }
         }
-        return "NO";
     }
 }
