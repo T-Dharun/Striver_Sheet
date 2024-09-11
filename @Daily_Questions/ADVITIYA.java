68c7727cf9448816becfff57d8994d6afe8fab25
T-Dharun <dharuneee007@gmail.com>
2024-09-11 11:03:20 +0530

    Day1 BYTS

---
+++ @Daily_Questions/ADVITIYA.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if((n>>4)!=0) System.out.println("ADVITIYA");
+		else System.out.println("WAITING FOR ADVITIYA");
+
+	}
+}
