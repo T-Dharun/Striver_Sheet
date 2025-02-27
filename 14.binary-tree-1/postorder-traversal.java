b9a75bb707828bc5aaae774f0ef578ade987e58b
T-Dharun <dharuneee007@gmail.com>
2025-02-27 19:14:10 +0530

    tree-1: in-pre-postorder

---
+++ 14.binary-tree-1/postorder-traversal.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void postorder(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        postorder(root.left,list);
+        postorder(root.right,list);
+        list.add(root.val);
+    }
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list=new ArrayList<>();
+        postorder(root,list);
+        return list;
+    }
+}
